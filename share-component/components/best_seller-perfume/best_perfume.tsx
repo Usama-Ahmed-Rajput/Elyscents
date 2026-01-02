a70886bb7539558bd9e0a4c5f77ad3b484dd8dbb
Usama-Ahmed-Rajput <umerusama871@gmail.com>
2026-01-02 21:32:31 +0500

    update

--- share-component/components/best_seller-perfume/best_perfume.tsx
+++ share-component/components/best_seller-perfume/best_perfume.tsx
@@ -1,8 +1,21 @@
-"use client";
-import React, { useState } from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 import style from './best_perfume.module.scss'
 const Best_perfume = () => {
     const [hoverIndex, setHoverIndex] = useState<any>(null);
+    const [isMobile, setIsMobile] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768)
+        }
+
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
     let products = [
         {
             name: 'Royal Oud',
@@ -62,34 +75,59 @@ const Best_perfume = () => {
         }
 
     ];
+    const CartItem = (value: any, index: number) => (
+        <div className={style.perfume_item} key={index}>
+            <img
+                src={hoverIndex === index ? value.src1 : value.src}
+                onMouseEnter={() => setHoverIndex(index)}
+                onMouseLeave={() => setHoverIndex(null)}
+                alt=""
+            />
+
+            <h3 className={style.deal_names}>{value.name}</h3>
+
+            <div>
+                <span className={style.cut_price}>Rs.{value.cut_price}.00</span>
+                <span className={style.real_price}>Rs.{value.real_price}.00</span>
+            </div>
+
+            <div>
+                <span className={style.save_price}>
+                    Save Rs.{value.cut_price - value.real_price}.00
+                </span>
+            </div>
+
+            <div className={style.cart_btn}>
+                <button>Add to Cart</button>
+            </div>
+        </div>
+    )
+
     return (
         <>
+           
             <div className={style.wrapper}>
-                <h2 className={style.heading}>Best Seller Perfumes</h2>
+                <h2 className={style.heading}>BEST SELLERS PERFUMES</h2>
             </div>
-            <div className={style.perfumes}>
-                {products.map((value, index) => (
 
-                    <div className={style.perfume_item} key={index}>
+            {!isMobile && (
+                <div className={style.perfumes}>
+                    {products.map((value, index) =>
+                        CartItem(value, index)
+                    )}
+                </div>
+            )}
 
-                        <img src={hoverIndex === index ? value.src1 : value.src}
-                            onMouseEnter={() => setHoverIndex(index)}
-                            onMouseLeave={() => setHoverIndex(null)}
-                            alt="" />
-                        <h3 className={style.deal_names}>{value.name}</h3>
-                        <div>
-                            <span className={style.cut_price}>Rs.{value.cut_price}.00</span>
-                            <span className={style.real_price}>Rs.{value.real_price}.00</span>
-                        </div>
-                        <div>
-                            <span className={style.save_price}>Save Rs.{value.cut_price - value.real_price}.00</span>
-                        </div>
-                        <div className={style.cart_btn}><button>Add to Cart</button></div>
-                    </div>
-                ))}
+            {isMobile && (
+                <Swiper slidesPerView={1.2} spaceBetween={15}>
+                    {products.map((value, index) => (
+                        <SwiperSlide key={index}>
+                            {CartItem(value, index)}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            )}
 
-
-            </div>
 
 
             <div className={style.view_all_btn}><button>VIEW ALL</button></div>
